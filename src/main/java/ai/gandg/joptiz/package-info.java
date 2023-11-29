6ff1358fcf51eaab39bb5f25cd3f13f72d6c0395
Gabor Balazs <gabalz@gmail.com>
2023-11-29 13:32:03 +0100

    Package documentation.

---
+++ src/main/java/ai/gandg/joptiz/package-info.java
@@ -0,0 +1,15 @@
+
+/**
+ * This package provides the JOptiZ optimization library. It intends to
+ * <ul>
+ *   <li>support garbage collection free operations;</li>
+ *   <li>rely on minimal dependencies;</li>
+ *   <li>and stay compilable on embedded Java implementations.</li>
+ * </ul>
+ *
+ * Supported optimization algorithms:
+ * <ul>
+ *   <li>Limited-memory Broyden–Fletcher–Goldfarb–Shanno (L-BFGS) algorithm.</li>
+ * </ul>
+ */
+package ai.gandg.joptiz;
